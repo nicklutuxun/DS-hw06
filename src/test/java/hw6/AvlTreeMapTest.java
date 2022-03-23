aa4bac1ff63052d10ba21c87551827e547892927
Nick Lu <tlu32@jhu.edu>
2022-03-23 10:31:18 -0400

    Add tests

--- src/test/java/hw6/AvlTreeMapTest.java
+++ src/test/java/hw6/AvlTreeMapTest.java
@@ -50,5 +50,99 @@ public class AvlTreeMapTest extends BinarySearchTreeMapTest {
   }
 
   // TODO Add more tests
-
+  @Test
+  public void insertRightRotation() {
+    map.insert("3", "c");
+    // System.out.println(avl.toString());
+    // must print
+    /*
+        3:c
+     */
+  
+    map.insert("2", "b");
+    // System.out.println(avl.toString());
+    // must print
+    /*
+        3:c,
+        2:b null
+     */
+  
+    map.insert("1", "a"); // it must do a right rotation here!
+    // System.out.println(avl.toString());
+    // must print
+    /*
+        2:b,
+        1:a 3:c
+     */
+  
+    String[] expected = new String[]{
+        "2:b",
+        "1:a 3:c"
+    };
+    assertEquals((String.join("\n", expected) + "\n"), map.toString());
+  }
+  
+  @Test
+  public void insertRightLeftRotation() {
+    map.insert("1", "a");
+    // System.out.println(avl.toString());
+    // must print
+    /*
+        1:a
+     */
+    
+    map.insert("3", "c");
+    // System.out.println(avl.toString());
+    // must print
+    /*
+        1:a,
+        null 3:c
+     */
+    
+    map.insert("2", "b"); // it must do a right-left rotation here!
+    // System.out.println(avl.toString());
+    // must print
+    /*
+        2:b,
+        1:a 3:c
+     */
+    
+    String[] expected = new String[]{
+        "2:b",
+        "1:a 3:c"
+    };
+    assertEquals((String.join("\n", expected) + "\n"), map.toString());
+  }
+  
+  @Test
+  public void insertLeftRightRotation() {
+    map.insert("3", "c");
+    // System.out.println(avl.toString());
+    // must print
+    /*
+        3:c
+     */
+    
+    map.insert("1", "a");
+    // System.out.println(avl.toString());
+    // must print
+    /*
+        3:c,
+        1:a null
+     */
+    
+    map.insert("2", "b"); // it must do a left-right rotation here!
+    // System.out.println(avl.toString());
+    // must print
+    /*
+        2:b,
+        1:a 3:c
+     */
+    
+    String[] expected = new String[]{
+        "2:b",
+        "1:a 3:c"
+    };
+    assertEquals((String.join("\n", expected) + "\n"), map.toString());
+  }
 }
