0e9c5eb95c695d78fa181bbf9e765a7d2c0c5311
Nick Lu <tlu32@jhu.edu>
2022-03-29 14:38:09 -0400

    Add tests

--- src/test/java/hw6/AvlTreeMapTest.java
+++ src/test/java/hw6/AvlTreeMapTest.java
@@ -53,28 +53,9 @@ public class AvlTreeMapTest extends BinarySearchTreeMapTest {
   @Test
   public void insertRightRotation() {
     map.insert("3", "c");
-    // System.out.println(avl.toString());
-    // must print
-    /*
-        3:c
-     */
-  
     map.insert("2", "b");
-    // System.out.println(avl.toString());
-    // must print
-    /*
-        3:c,
-        2:b null
-     */
-  
     map.insert("1", "a"); // it must do a right rotation here!
-    // System.out.println(avl.toString());
-    // must print
-    /*
-        2:b,
-        1:a 3:c
-     */
-  
+
     String[] expected = new String[]{
         "2:b",
         "1:a 3:c"
@@ -85,27 +66,8 @@ public class AvlTreeMapTest extends BinarySearchTreeMapTest {
   @Test
   public void insertRightLeftRotation() {
     map.insert("1", "a");
-    // System.out.println(avl.toString());
-    // must print
-    /*
-        1:a
-     */
-    
     map.insert("3", "c");
-    // System.out.println(avl.toString());
-    // must print
-    /*
-        1:a,
-        null 3:c
-     */
-    
     map.insert("2", "b"); // it must do a right-left rotation here!
-    // System.out.println(avl.toString());
-    // must print
-    /*
-        2:b,
-        1:a 3:c
-     */
     
     String[] expected = new String[]{
         "2:b",
@@ -117,27 +79,8 @@ public class AvlTreeMapTest extends BinarySearchTreeMapTest {
   @Test
   public void insertLeftRightRotation() {
     map.insert("3", "c");
-    // System.out.println(avl.toString());
-    // must print
-    /*
-        3:c
-     */
-    
     map.insert("1", "a");
-    // System.out.println(avl.toString());
-    // must print
-    /*
-        3:c,
-        1:a null
-     */
-    
     map.insert("2", "b"); // it must do a left-right rotation here!
-    // System.out.println(avl.toString());
-    // must print
-    /*
-        2:b,
-        1:a 3:c
-     */
     
     String[] expected = new String[]{
         "2:b",
@@ -145,4 +88,124 @@ public class AvlTreeMapTest extends BinarySearchTreeMapTest {
     };
     assertEquals((String.join("\n", expected) + "\n"), map.toString());
   }
+  
+  @Test
+  public void insertNoRotation() {
+    map.insert("2", "b");
+    map.insert("1", "a");
+    map.insert("3", "c"); // no rotation here!
+ 
+    String[] expected = new String[]{
+        "2:b",
+        "1:a 3:c"
+    };
+    assertEquals((String.join("\n", expected) + "\n"), map.toString());
+  }
+  
+  @Test
+  public void removeLeafLeftRotation() {
+    map.insert("2", "b");
+    map.insert("1", "a");
+    map.insert("3", "c"); // no rotation here!
+
+    String[] expected = new String[]{
+        "2:b",
+        "1:a 3:c"
+    };
+    assertEquals((String.join("\n", expected) + "\n"), map.toString());
+  }
+  
+  @Test
+  public void removeLeafRightRotation() {
+  
+  }
+  
+  @Test
+  public void removeLeafLeftRightRotation() {
+  
+  }
+  
+  @Test
+  public void removeLeafRightLeftRotation() {
+  
+  }
+  
+  @Test
+  public void removeOneChildLeftRotation() {
+  
+  }
+  
+  @Test
+  public void removeOneChildRightRotation() {
+  
+  }
+  
+  @Test
+  public void removeOneChildLeftRightRotation() {
+  
+  }
+  
+  @Test
+  public void removeOneChildRightLeftRotation() {
+  
+  }
+  
+  @Test
+  public void removeTwoChildrenLeftRotation() {
+  
+  }
+  
+  @Test
+  public void removeTwoChildrenRightRotation() {
+  
+  }
+  
+  @Test
+  public void removeTwoChildrenLeftRightRotation() {
+  
+  }
+  
+  @Test
+  public void removeTwoChildrenRightLeftRotation() {
+  
+  }
+  
+  @Test
+  public void removeLeafNoRotation() {
+  
+  }
+  
+  @Test
+  public void removeOneChildNoRotation() {
+  
+  }
+  
+  @Test
+  public void removeTwoChildrenNoRotation() {
+  
+  }
+  
+  @Test
+  public void ExerciseFromWebpage() {
+    map.insert("15", "15");
+    map.insert("20", "20");
+    map.insert("24", "24");
+    map.insert("10", "10");
+    map.insert("13", "13");
+    map.insert("07", "07");
+    map.insert("30", "30");
+    map.insert("36", "36");
+    map.insert("25", "25");
+    map.remove("24");
+    map.remove("20");
+    String[] expected = new String[]{
+        "13:13",
+        "10:10 30:30",
+        "07:07 null 15:15 36:36",
+        "null null null null null 25:25 null null"
+    };
+    assertEquals((String.join("\n", expected) + "\n"), map.toString());
+  }
+  
+  
 }
