170bff563b3aed2dcda926391af8237d2dbb0c69
Nick Lu <tlu32@jhu.edu>
2022-03-29 18:18:24 -0400

    Implementation

--- src/main/java/hw6/bst/TreapMap.java
+++ src/main/java/hw6/bst/TreapMap.java
@@ -3,6 +3,7 @@ package hw6.bst;
 import hw6.OrderedMap;
 import java.util.Iterator;
 import java.util.Random;
+import java.util.Stack;
 
 /**
  * Map implemented as a Treap.
@@ -16,6 +17,7 @@ public class TreapMap<K extends Comparable<K>, V> implements OrderedMap<K, V> {
   private static Random rand;
   /*** Do not change variable name of 'root'. ***/
   private Node<K, V> root;
+  private int size;
 
   /**
    * Make a TreapMap.
@@ -23,45 +25,197 @@ public class TreapMap<K extends Comparable<K>, V> implements OrderedMap<K, V> {
   public TreapMap() {
     rand = new Random();
   }
-
+  
+  /**
+   * Make a TreapMap with customized seed.
+   * @param seed Seed for random
+   */
+  public TreapMap(int seed) {
+    rand = new Random(seed);
+  }
+  
+  private Node<K, V> insert(Node<K, V> n, K k, V v) {
+    if (n == null) {
+      return new Node<>(k, v);
+    }
+    
+    int cmp = k.compareTo(n.key);
+    if (cmp < 0) {
+      n.left = insert(n.left, k, v);
+    } else if (cmp > 0) {
+      n.right = insert(n.right, k, v);
+    } else {
+      throw new IllegalArgumentException("duplicate key " + k);
+    }
+    
+    n = balance(n);
+    
+    return n;
+  }
+  
   @Override
   public void insert(K k, V v) throws IllegalArgumentException {
-    // TODO Implement Me!
+    if (k == null) {
+      throw new IllegalArgumentException("cannot handle null key");
+    }
+    root = insert(root, k, v);
+    size++;
   }
-
-  @Override
-  public V remove(K k) throws IllegalArgumentException {
-    // TODO Implement Me!
+  
+  private Node<K,V> balance(Node<K,V> n) {
+    if (n.left != null && n.left.priority < n.priority) {
+      n = rightRotation(n);
+    } else if (n.right != null && n.right.priority < n.priority) {
+      n = leftRotation(n);
+    }
+    return n;
+  }
+  
+  // Return node for given key,
+  // throw an exception if the key is not in the tree.
+  private Node<K, V> findForSure(K k) {
+    Node<K, V> n = find(k);
+    if (n == null) {
+      throw new IllegalArgumentException("cannot find key " + k);
+    }
+    return n;
+  }
+  
+  private Node<K, V> find(K k) {
+    if (k == null) {
+      throw new IllegalArgumentException("cannot handle null key");
+    }
+    Node<K, V> n = root;
+    while (n != null) {
+      int cmp = k.compareTo(n.key);
+      if (cmp < 0) {
+        n = n.left;
+      } else if (cmp > 0) {
+        n = n.right;
+      } else {
+        return n;
+      }
+    }
     return null;
   }
-
+  
+  @Override
+  public V remove(K k) throws IllegalArgumentException {
+    Node<K, V> node = findForSure(k);
+    V value = node.value;
+    root = remove(root, node);
+    size--;
+    return value;
+  }
+  
+  // Remove node with given key from subtree rooted at given node;
+  // Return changed subtree with given key missing.
+  private Node<K, V> remove(Node<K, V> subtreeRoot, Node<K, V> toRemove) {
+    int cmp = subtreeRoot.key.compareTo(toRemove.key);
+    if (cmp == 0) {
+      return remove(subtreeRoot);
+    } else if (cmp > 0) {
+      subtreeRoot.left = remove(subtreeRoot.left, toRemove);
+    } else {
+      subtreeRoot.right = remove(subtreeRoot.right, toRemove);
+    }
+    
+    subtreeRoot = balance(subtreeRoot);
+    
+    return subtreeRoot;
+  }
+  
+  // Remove given node and return the remaining tree (structural change).
+  private Node<K, V> remove(Node<K, V> node) {
+    // Easy if the node has 0 or 1 child.
+    if (node.right == null) {
+      return node.left;
+    } else if (node.left == null) {
+      return node.right;
+    }
+    
+    if (node.left.priority > node.right.priority) {
+      node = leftRotation(node);
+      node.left = remove(node.left);
+    } else if (node.left.priority < node.right.priority) {
+      node = rightRotation(node);
+      node.right = remove(node.right);
+    }
+    
+    return node;
+  }
+  
+  private Node<K,V> leftRotation(Node<K,V> n) {
+    Node<K,V> child = n.right;
+    n.right = child.left;
+    child.left = n;
+    return child;
+  }
+  
+  private Node<K,V> rightRotation(Node<K,V> n) {
+    Node<K,V> child = n.left;
+    n.left = child.right;
+    child.right = n;
+    return child;
+  }
+  
   @Override
   public void put(K k, V v) throws IllegalArgumentException {
-    // TODO Implement Me!
+    Node<K, V> n = findForSure(k);
+    n.value = v;
   }
 
   @Override
   public V get(K k) throws IllegalArgumentException {
-    // TODO Implement Me!
-    return null;
+    Node<K, V> n = findForSure(k);
+    return n.value;
   }
 
   @Override
   public boolean has(K k) {
-    // TODO Implement Me!
-    return false;
+    if (k == null) {
+      return false;
+    }
+    return find(k) != null;
   }
 
   @Override
   public int size() {
-    // TODO Implement Me!
-    return 0;
+    return size;
   }
 
   @Override
   public Iterator<K> iterator() {
-    // TODO Implement Me!
-    return null;
+    return new InorderIterator();
+  }
+  
+  // Iterative in-order traversal over the keys
+  private class InorderIterator implements Iterator<K> {
+    private final Stack<Node<K, V>> stack;
+    
+    InorderIterator() {
+      stack = new Stack<>();
+      pushLeft(root);
+    }
+    
+    private void pushLeft(Node<K, V> curr) {
+      while (curr != null) {
+        stack.push(curr);
+        curr = curr.left;
+      }
+    }
+    
+    @Override
+    public boolean hasNext() {
+      return !stack.isEmpty();
+    }
+    
+    @Override
+    public K next() {
+      Node<K, V> top = stack.pop();
+      pushLeft(top.right);
+      return top.key;
+    }
   }
 
   /*** Do not change this function's name or modify its code. ***/
