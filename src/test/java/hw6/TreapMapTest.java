96c65483b4281353c8cd4bc46a1c018ae1b7b30c
Nick Lu <tlu32@jhu.edu>
2022-03-29 16:48:46 -0400

    Add tests

--- src/test/java/hw6/TreapMapTest.java
+++ src/test/java/hw6/TreapMapTest.java
@@ -1,6 +1,9 @@
 package hw6;
 
 import hw6.bst.TreapMap;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * In addition to the tests in BinarySearchTreeMapTest (and in OrderedMapTest & MapTest),
@@ -16,5 +19,44 @@ public class TreapMapTest extends BinarySearchTreeMapTest {
 
   // TODO Add tests
   //  (think about how you might write tests while randomness is involved in TreapMap implementation!)
-
+  
+  @Test
+  public void insertLeftRotation() {
+  
+  }
+  
+  @Test
+  public void insertRightRotation() {
+  
+  }
+  
+  @Test
+  public void removeOneChildLeftRotation() {
+  
+  }
+  
+  @Test
+  public void removeOneChildRightRotation() {
+  
+  }
+  
+  @Test
+  public void removeTwoChildrenLeftRotation() {
+  
+  }
+  
+  @Test
+  public void removeTwoChildrenRightRotation() {
+  
+  }
+  
+  @Test
+  public void removeLeafNoRotation() {
+  
+  }
+  
+  @Test
+  public void insertNoRotation() {
+  
+  }
 }
